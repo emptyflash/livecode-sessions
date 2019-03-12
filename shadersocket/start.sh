fe0e304d990b4e0bbdca296be7b4057271f92caf
Cameron Alexander <emptyflash@gmail.com>
2019-03-11 20:51:54 -0500

    WIP for audio input

--- shadersocket/start.sh
+++ shadersocket/start.sh
@@ -2,6 +2,9 @@
 
 trap 'kill $(jobs -p)' EXIT
 script_dir=$(dirname "${BASH_SOURCE[0]}")
-ln -s $(pwd)/shader.frag $script_dir/shader.frag
-cd $script_dir && python -m SimpleHTTPServer 8080 &
+shader_dir=$(pwd)
+ln -s $shader_dir/shader.frag $script_dir/shader.frag
+cd $script_dir
+python -m SimpleHTTPServer 8080 &
+cd $shader_dir
 websocketd --port 9000 $script_dir/watchcat.sh
--- shadersocket/std.frag
+++ shadersocket/std.frag
@@ -1,6 +1,7 @@
 precision mediump float;
 uniform float u_time;
 uniform vec2 u_resolution;
+uniform int u_audio[16];
 float random (in vec2 st) {
     return fract(sin(dot(st.xy,
                          vec2(12.9898,78.233)))*
