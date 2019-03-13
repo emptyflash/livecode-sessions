ac5d409659f87f4f49b1ee41fd3445cf469a2859
Cameron <emptyflash@gmail.com>
2019-03-13 18:03:14 -0400

    WIP caddy https setup

---
+++ 20190313-livecode/shader.frag
@@ -0,0 +1,4 @@
+void main() {
+    vec2 st = gl_FragCoord.xy / u_resolution.xy;
+    gl_FragColor = osc(st, 60., .1, 900. * u_audio[0]);
+}
--- shadersocket/start.sh
+++ shadersocket/start.sh
@@ -2,9 +2,11 @@
 
 export SHADER_FOLDER=$(pwd)
 cd $(dirname "${BASH_SOURCE[0]}")
-services="nginx watchcat"
+services="watchcat"
 if [ -f ~/.ngrok2/ngrok.yml ]; then
     export NGROK_AUTHTOKEN=$(docker run -v ~/.ngrok2:/workdir mikefarah/yq yq read ngrok.yml authtoken)
-    services="$services ngrok"
+    services="$services caddy ngrok"
+else
+    services="$services nginx"
 fi
 docker-compose up --build $services
