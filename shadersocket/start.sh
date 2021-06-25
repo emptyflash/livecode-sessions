9b95fae7a072807b9f537b87f4d415d5dec74335
Cameron Alexander <emptyflash@gmail.com>
2021-06-25 12:41:25 -0400

    Lots of sessions and new features

--- shadersocket/start.sh
+++ shadersocket/start.sh
@@ -4,7 +4,7 @@ export SHADER_FOLDER=$(pwd)
 cd $(dirname "${BASH_SOURCE[0]}")
 services="nginx watchcat"
 if [ -f ~/.ngrok2/ngrok.yml ]; then
-    export NGROK_AUTHTOKEN=$(docker run -v ~/.ngrok2:/workdir mikefarah/yq yq read ngrok.yml authtoken)
+    export NGROK_AUTHTOKEN=$(docker run -v ~/.ngrok2:/workdir mikefarah/yq:2 yq read ngrok.yml authtoken)
     services="$services ngrok"
 fi
 docker-compose up --build $services
