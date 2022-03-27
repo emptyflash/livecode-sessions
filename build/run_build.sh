d2fce546b9ac6ce0f335bed418ecc8a77290305b
cameron <emptyflash@gmail.com>
2022-03-27 15:14:07 -0400

    Still debugging

--- build/run_build.sh
+++ build/run_build.sh
@@ -18,5 +18,6 @@ for d in */ ; do
     if [[ ! -f "$d/index.html" ]]; then
         echo "Copying index to $d"
         cp ./shadersocket/index.html "$d/index.html"
+        cp ./shadersocket/puppy.jpg "$d/puppy.jpg"
     fi
 done
