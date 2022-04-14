6a2fabc523b2db9510bc75899587e02588ca6ada
Cameron Alexander <emptyflash@gmail.com>
2022-04-13 22:36:27 -0400

    Start working on peaceblvck set

--- shadersocket/start.sh
+++ shadersocket/start.sh
@@ -7,5 +7,4 @@ if [ -f ~/.ngrok2/ngrok.yml ]; then
     export NGROK_AUTHTOKEN=$(docker run -v ~/.ngrok2:/workdir mikefarah/yq:2 yq read ngrok.yml authtoken)
     services="$services ngrok"
 fi
-docker-compose up --build -d $services
-docker-compose logs -f watchcat
+docker-compose up --build $services
