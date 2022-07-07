1e8367aa72e71456b063bfe63fefe77210eb397a
Cameron Alexander <emptyflash@gmail.com>
2022-07-07 13:35:24 -0400

    More sessions

---
+++ hydra-2022-7-7-12.14.59.js
@@ -0,0 +1,11 @@
+await loadScript("https://cdn.statically.io/gl/metagrowing/extra-shaders-for-hydra/main/lib/all.js")
+
+osc(2, 1, 127)
+	.kaleid(10)
+	.mask(warp(1, .2))
+	.saturate()
+	.diff(src(o0)
+        .colorama(0.02)
+		.scale(1.01)
+         , 0.25)
+	.out()
