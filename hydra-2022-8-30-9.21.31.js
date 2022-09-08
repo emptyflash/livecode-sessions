eeee1386e7829bc7eb73a34c8d789035cb82f66f
Cameron Alexander <emptyflash@gmail.com>
2022-09-08 15:28:04 -0400

    Add transposition sketch

---
+++ hydra-2022-8-30-9.21.31.js
@@ -0,0 +1,19 @@
+// Transposition
+// by Cameron Alexander (https://emptyfla.sh)
+
+(async () => {
+await loadScript("https://cdn.statically.io/gl/emptyflash/extra-shaders-for-hydra/9fb1c76e8/lib/all.js")
+
+let frame = 0;
+
+src(o0)
+	.sort(0, () => frame++, [0, -1, -1, 1].fast(0.5), [1, -1, 0, 1].fast(0.5))
+	.layer(osc(5, .3, 2)
+		.mask(shape(99, () => 0.4 + 0.2 * Math.sin(time))
+			.rotate(.1, .1)
+			.scrollX(.1, .1)
+			.scrollY(.1, .05)
+		))
+	.modulateHue(o0, 500)
+	.out()
+})()
