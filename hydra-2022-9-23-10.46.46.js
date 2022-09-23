55b338304e0911c3031c059588b3f4aad6d547df
Cameron Alexander <emptyflash@gmail.com>
2022-09-23 11:56:02 -0400

    fix

--- hydra-2022-9-23-10.46.46.js
+++ hydra-2022-9-23-10.46.46.js
@@ -1,3 +1,4 @@
+(async () => {
 await loadScript("https://livecode.emptyfla.sh/hydra/all.js")
 updateTextureParams(o0, {wrap: ["mirror", "mirror"]})
 src2(o0)
@@ -5,3 +6,4 @@ src2(o0)
 	.scale(0.9, 16/9)
 .rotate(()=>Math.abs(Math.sin(time*0.01))*Math.PI*2+1,0)
 	.out()
+})()
