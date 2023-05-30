92be5a7fecff3207db90a1e8c362d48cdcbc35a2
Cameron Alexander <emptyflash@gmail.com>
2023-05-30 11:44:39 -0400

    Update midi

--- hydra/lib/register-midi.js
+++ hydra/lib/register-midi.js
@@ -1,8 +1,7 @@
-// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
-// see: https://github.com/hydra-synth/hydra/blob/main/docs/midi.md
 // register WebMIDI
 navigator.requestMIDIAccess()
     .then(onMIDISuccess, onMIDIFailure);
+
 function onMIDISuccess(midiAccess) {
     console.log(midiAccess);
     var inputs = midiAccess.inputs;
@@ -11,15 +10,18 @@ function onMIDISuccess(midiAccess) {
         input.onmidimessage = getMIDIMessage;
     }
 }
+
 function onMIDIFailure() {
     console.log('Could not access your MIDI devices.');
 }
+
 //create an array to hold our cc values and init to a normalized value
-cc=Array(128).fill(0.5)
+window.cc = Array(128).fill(0)
+
 getMIDIMessage = function(midiMessage) {
-    var arr = midiMessage.data
+    var arr = midiMessage.data    
     var index = arr[1]
+    //console.log('Midi received on cc#' + index + ' value:' + arr[2])    // uncomment to monitor incoming Midi
     var val = (arr[2]+1)/128.0  // normalize CC values to 0.0 - 1.0
-		console.log('Midi received on cc#' + index + ' value:' + arr[2] + ' ' + val)    // uncomment to monitor incoming Midi
     cc[index]=val
 }
